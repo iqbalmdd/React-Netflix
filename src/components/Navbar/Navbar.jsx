d3344a154b2efcbf09cb127b2992fcc1517d3321
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-09 10:58:30 +0700

    [ADD] search and nav position

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import DarkMode from '../DarkMode/DarkMode'
-import "./Navbar.css"
-import Fire from "../../assets/images/fire.png"
-import Star from "../../assets/images/glowing-star.png"
-import Party from "../../assets/images/partying-face.png"
+import React from "react";
+import DarkMode from "../DarkMode/DarkMode";
+import "./Navbar.css";
+import Search from "../../assets/images/search.png";
+import Profile from "../../assets/images/SAYA.jpg";
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-        <h1>Bale Movie</h1>
+    <nav className="navbar">
+      <div className="nav_left">
+        <h1>BaleMovie</h1>
         <div className="navbar_links">
-            <DarkMode />
-            <a href="#popular">
-                Popular
-                <img className='navbar_emoji' src={Fire} alt="fire emoji" />
-            </a>
-            <a href="#top_rated">
-                Top Rated
-                <img className='navbar_emoji' src={Star} alt="star emoji" />
-            </a>
-            <a href="#upcoming">
-                Upcoming
-                <img className='navbar_emoji' src={Party} alt="party emoji" />
-            </a>
+          
+          <a href="#popular">Popular</a>
+          <a href="#top_rated">Top Rated</a>
+          <a href="#upcoming">Upcoming</a>
         </div>
+      </div>
+      <div className="nav-right align_center">
+        <DarkMode />
+        <img className="search_image" src={Search} alt="" />
+        <img className="profile_image" src={Profile} alt="" />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
