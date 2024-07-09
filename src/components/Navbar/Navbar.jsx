cc7d2e33d3a51e81211635a9ad58a65938dac7e4
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-09 12:00:24 +0700

    [ADD] search bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,62 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DarkMode from "../DarkMode/DarkMode";
 import "./Navbar.css";
 import Search from "../../assets/images/search.png";
 import Profile from "../../assets/images/SAYA.jpg";
 
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState("#popular")
+  const [activeLink, setActiveLink] = useState("#popular");
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const toggleSearchBar = () => {
+    setShowSearchBar(!showSearchBar);
+  };
 
   const handleClick = (id) => {
-    setActiveLink(id)
-  }
+    setActiveLink(id);
+  };
 
   return (
     <nav className="navbar">
       <div className="nav_left">
         <h1>BaleMovie</h1>
         <div className="navbar_links">
-          
-          <a href="#popular" onClick={() => handleClick("#popular")} className={activeLink === "#popular" ? "active" : ""}>Popular</a>
-          <a href="#top_rated" onClick={() => handleClick("#top_rated")} className={activeLink === "#top_rated" ? "active" : ""}>Top Rated</a>
-          <a href="#upcoming" onClick={() => handleClick("#upcoming")} className={activeLink === "#upcoming" ? "active" : ""}>Upcoming</a>
+          <a
+            href="#popular"
+            onClick={() => handleClick("#popular")}
+            className={activeLink === "#popular" ? "active" : ""}
+          >
+            Popular
+          </a>
+          <a
+            href="#top_rated"
+            onClick={() => handleClick("#top_rated")}
+            className={activeLink === "#top_rated" ? "active" : ""}
+          >
+            Top Rated
+          </a>
+          <a
+            href="#upcoming"
+            onClick={() => handleClick("#upcoming")}
+            className={activeLink === "#upcoming" ? "active" : ""}
+          >
+            Upcoming
+          </a>
         </div>
       </div>
       <div className="nav-right align_center">
         <DarkMode />
-        <img className="search_image" src={Search} alt="" />
+        {showSearchBar && (
+          <div className="search_bar">
+            <input type="text" placeholder="Search..." />
+          </div>
+        )}
+        <img
+          className="search_image"
+          src={Search}
+          alt=""
+          onClick={toggleSearchBar}
+        />
         <img className="profile_image" src={Profile} alt="" />
       </div>
     </nav>
